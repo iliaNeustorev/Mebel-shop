fd5f0b95f5f4b4c4e6468f13bc7cf37e68126444
ilia <jjnn95@yandex.ru>
2022-01-27 18:18:34 +0300

    ordercontroller

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    public function index () 
+    {
+        return view('orders');
+    }
+
+    public function repeatOrder ($id) 
+    {
+        $order = Order::find($id)->products;
+        
+        $basket_products = $order->map( function ($productOrder) {
+            $product = Product::find($productOrder->id);
+            return [
+                'title' => $product->name,
+                'price' => $product->price,
+                'quantity' => $productOrder->pivot->quantity,    
+            ];
+
+        });
+        session()->put('products', $basket_products);
+        session()->save();
+        $date = [
+            'products' =>  $basket_products,
+            'title' => 'Корзина',
+            'sum_order' => 12,
+        ];
+        return redirect()->route('basket',$date);
+    }
+}
